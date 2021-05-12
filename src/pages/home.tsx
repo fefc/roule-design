dfccf65223812296cb7b9be7befbe5ed00832a49
Felix Chantrel <felchantrel@gmail.com>
2021-05-12 22:58:23 +0200

    Better home design.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,36 +7,26 @@ const HomePage = () => {
 
   return (
     <>
-      <style type="text/css">
-      {`
-        .my-row {
-          height: 100%;
-        }
-
-        .my-col {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-      `}
-      </style>
       <Container fluid style={{height: '100%', backgroundImage: `url(${background})`, backgroundPosition: 'center', backgroundSize: 'cover'}}>
-        <Row className="my-row">
-          <Col className="my-col">
-            <Button variant="primary" as={Link} to={'/categories'}>Explorez</Button>
-          </Col>
-          <Col className="my-col">
+        <Row style={{height: '100%'}}>
+          <Col className="d-flex justify-content-center align-items-center" lg={6} md={{ order: 2 }}>
             <Card>
+              <Card.Header>A propos</Card.Header>
               <Card.Body>
-                <Card.Title>A propos de Roule Design</Card.Title>
-                <Card.Text>
-                Se quid nulla ea senserit est excepteur malis quid probant illum e eu aute enim
-                aut mandaremus, ut minim culpa elit arbitror o summis aut ullamco non noster ab
-                consequat amet ubi mentitum reprehenderit de laboris export expetendis ullamco,
-                aliqua te ingeniis.
-                </Card.Text>
+                  <p>
+                    Se quid nulla ea senserit est excepteur malis quid probant illum e eu aute enim
+                    aut mandaremus, ut minim culpa elit arbitror o summis aut ullamco non noster ab
+                    consequat amet ubi mentitum reprehenderit de laboris export expetendis ullamco,
+                    aliqua te ingeniis.
+                  </p>
+                  <footer className="blockquote-footer">
+                    Ce
+                  </footer>
               </Card.Body>
             </Card>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center" lg={6} md={{ order: 1 }}>
+            <Button variant="primary" size="lg" as={Link} to={'/categories'}>Explorez</Button>
           </Col>
         </Row>
       </Container>
